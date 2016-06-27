27ad38fc7a99036e4e4d9db0aa616db8f1fede9a
SLS <iMac_Mobile_1@sanook-imac.local>
2016-06-27 13:54:05 +0700

    Initial Commit

---
+++ Initiative/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Initiative
+//
+//  Created by SLS on 6/27/16.
+//  Copyright © 2016 SLS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
